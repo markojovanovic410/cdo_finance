bc7783805116a30824956bc600afb12cd1352815
GoldSoft2021 <78613623+GoldSoft2021@users.noreply.github.com>
2021-04-28 15:16:14 +0700

    update by dmitrii

--- src/components/tranchePools/index.tsx
+++ src/components/tranchePools/index.tsx
@@ -1,3 +1,9 @@
 import './index.scss';
 
 export {YourPositions} from './YourPositions'
+export {DepositModal} from './DepositModal'
+export {TransferModal} from './TransferModal'
+export {RedeemModal} from './RedeemModal'
+export {WithdrawalModal} from './WithdrawalModal'
+export {WithdrawalStep2Modal} from './WithdrawalStep2Modal'
+export {DepositStep2Modal} from './DepositStep2Modal'
