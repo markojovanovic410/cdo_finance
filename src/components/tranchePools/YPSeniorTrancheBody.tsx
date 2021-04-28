9da20d5c23acde14ba86f0d2f42ad71e6a86698a
GoldSoft2021 <78613623+GoldSoft2021@users.noreply.github.com>
2021-04-28 15:17:55 +0700

    update by dmitrii

--- src/components/tranchePools/YPSeniorTrancheBody.tsx
+++ src/components/tranchePools/YPSeniorTrancheBody.tsx
@@ -2,8 +2,14 @@ import React, {useState, useEffect, useRef} from 'react'
 import { Tab, Nav, Modal } from 'react-bootstrap';
 import { Button } from '../button';
 import DataTable from 'react-data-table-component';
+import { RedeemModal } from './RedeemModal';
+import { TransferModal } from './TransferModal';
 
 export const YPSeniorTrancheBody = () => {
+
+    const fee = "100";
+    const maturity_date = "12/11/2020"
+    const address_str = "123 Apple Road, Singapore"
   
   const ap_data = [
     { id: 1, platform: 'Autofarm', token: 'BNB', token_img:'/images/icons/bnb.svg', deposits:'2,779,040.29', redemable: '2,779,040.29', apy:2.12, time_left: '2 days 12 hrs 4 min 5 sec' }, 
@@ -43,7 +49,7 @@ export const YPSeniorTrancheBody = () => {
     },
     {
         name: '',
-        cell: () => <div className="td-actions ml-auto"><span className="btn-transfer">Transfer</span><Button>Redeem</Button></div>,
+        cell: (row:any) => <div className="td-actions ml-auto"><span className="btn-transfer"  onClick={() => handleTransferModalShow(row.platform, row.token_img, row.token, row.redemable, row.deposits, row.apy, maturity_date, address_str)}>Transfer</span><Button onClick={() => handleRedeemModalShow(row.platform, row.token_img, row.token, row.redemable, fee)}>Redeem</Button></div>,
         grow: 1.8,
     },
   ];
@@ -82,9 +88,46 @@ export const YPSeniorTrancheBody = () => {
         selector: 'transaction_time',
     },
   ];
+  const [redeemModalShow, setRedeemModalShow] = useState(false);
+  const [transferModalShow, setTransferModalShow] = useState(false);
+  const [tokenPlatform, setTokenPlatform] = useState("");
+  const [tokenImage, setTokenImage] = useState("");
+  const [token, setToken] = useState("");
+  const [redeemableAmount, setRedeemableAmount] = useState("");
+  const [feeAmount, setFeeAmount] = useState("");
+  const [depositedAmount, setDepositedAmount] = useState("");
+  const [guaranteedApy, setGuaranteedApy] = useState(0);
+  const [maturityDate, setMaturityDate] = useState("");
+  const [address, setAddress] = useState("");
+  const handleRedeemModalShow = (platform: string, token_img: string, token: string, redeemable_amount: string, fee: string) => {
+    setTokenPlatform(platform);
+    setTokenImage(token_img);
+    setToken(token);
+    setRedeemableAmount(redeemable_amount);
+    setFeeAmount(fee);
+    setRedeemModalShow(true);
+  }
+  const handleTransferModalShow = (platform: string, token_img: string, token: string, redeemable_amount: string, deposited_amount: string, guaranteed_apy: number, maturity_date: string, address: string) => {
+    setTokenPlatform(platform);
+    setTokenImage(token_img);
+    setToken(token);
+    setRedeemableAmount(redeemable_amount);
+    setDepositedAmount(deposited_amount);
+    setGuaranteedApy(guaranteed_apy);
+    setMaturityDate(maturity_date);
+    setAddress(address);
+    setTransferModalShow(true);
+  }
+  const redeemModalCallback = (showModal: boolean) => {
+    setRedeemModalShow(showModal)
+  };
+  const transferModalCallback = (showModal: boolean) => {
+    setTransferModalShow(showModal)
+  };
 
 	return (
 		<>
+        <div>
 	    <Tab.Container defaultActiveKey="tab_activePositions">
           <Nav className="tab-list nav-seniorTranche">
               <Nav.Item>
@@ -116,6 +159,9 @@ export const YPSeniorTrancheBody = () => {
               </Tab.Pane>
           </Tab.Content>
       </Tab.Container>
+        <RedeemModal showModal={redeemModalShow} parentCallback = {redeemModalCallback} modalPlatform={tokenPlatform} modalImage={tokenImage} modalToken={token} redeemableAmount={redeemableAmount} feeAmount={feeAmount} />
+        <TransferModal showModal={transferModalShow} parentCallback = {transferModalCallback} modalPlatform={tokenPlatform} modalImage={tokenImage} modalToken={token} redeemableAmount={redeemableAmount} depositedAmount={depositedAmount} guaranteedApy={guaranteedApy} maturityDate={maturityDate} address={address} />
+      </div>
  		</>
 	);
 }
